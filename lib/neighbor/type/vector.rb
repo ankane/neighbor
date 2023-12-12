9065a185a57b65119758ffb44f62c1f4f8e8c78a
Andrew Kane <andrew@ankane.org>
2023-12-12 14:17:05 -0500

    Added todo [skip ci]

--- lib/neighbor/type/vector.rb
+++ lib/neighbor/type/vector.rb
@@ -4,6 +4,11 @@ module Neighbor
       def type
         :vector
       end
+
+      # TODO uncomment in 0.4.0
+      # def deserialize(value)
+      #   value[1..-1].split(",").map(&:to_f) unless value.nil?
+      # end
     end
   end
 end
