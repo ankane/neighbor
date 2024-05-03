5f0720b247024c677522b0a46b346e4601d4a174
Andrew Kane <andrew@ankane.org>
2024-05-03 14:48:42 -0700

    Added cast method for vector and halfvec

--- lib/neighbor/type/vector.rb
+++ lib/neighbor/type/vector.rb
@@ -5,6 +5,14 @@ module Neighbor
         :vector
       end
 
+      def cast(value)
+        if value.is_a?(Array)
+          "[#{value.join(",")}]"
+        else
+          super
+        end
+      end
+
       def deserialize(value)
         value[1..-1].split(",").map(&:to_f) unless value.nil?
       end
