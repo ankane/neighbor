f6a8f8ebf76ee927f8137a6df9fad114d8b03c30
Andrew Kane <andrew@ankane.org>
2024-05-04 15:16:28 -0700

    Improved bit tests

--- test/bit_test.rb
+++ test/bit_test.rb
@@ -2,20 +2,36 @@ require_relative "test_helper"
 
 class BitTest < Minitest::Test
   def test_hamming
-    Item.create!(id: 1, binary_embedding: "000")
-    Item.create!(id: 2, binary_embedding: "101")
-    Item.create!(id: 3, binary_embedding: "111")
+    create_bit_items
+    result = Item.find(1).nearest_neighbors(:binary_embedding, distance: "hamming").first(3)
+    assert_equal [2, 3], result.map(&:id)
+    assert_elements_in_delta [2, 3], result.map(&:neighbor_distance)
+  end
+
+  def test_hamming_scope
+    create_bit_items
     result = Item.nearest_neighbors(:binary_embedding, "101", distance: "hamming").first(5)
     assert_equal [2, 3, 1], result.map(&:id)
     assert_elements_in_delta [0, 1, 2], result.map(&:neighbor_distance)
   end
 
   def test_jaccard
-    Item.create!(id: 1, binary_embedding: "000")
-    Item.create!(id: 2, binary_embedding: "101")
-    Item.create!(id: 3, binary_embedding: "111")
+    create_bit_items
+    result = Item.find(2).nearest_neighbors(:binary_embedding, distance: "jaccard").first(3)
+    assert_equal [3, 1], result.map(&:id)
+    assert_elements_in_delta [1/3.0, 1], result.map(&:neighbor_distance)
+  end
+
+  def test_jaccard_scope
+    create_bit_items
     result = Item.nearest_neighbors(:binary_embedding, "100", distance: "jaccard").first(5)
     assert_equal [2, 3, 1], result.map(&:id)
     assert_elements_in_delta [0.5, 2/3.0, 1], result.map(&:neighbor_distance)
+  end
+
+  def create_bit_items
+    Item.create!(id: 1, binary_embedding: "000")
+    Item.create!(id: 2, binary_embedding: "101")
+    Item.create!(id: 3, binary_embedding: "111")
   end
 end
