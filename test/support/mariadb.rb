bde7c15931e0c40a6b240baaf7c4caded952ff95
Andrew Kane <andrew@ankane.org>
2024-08-31 21:33:47 -0700

    Improved test [skip ci]

--- test/support/mariadb.rb
+++ test/support/mariadb.rb
@@ -6,8 +6,11 @@ end
 
 MariadbRecord.connection.instance_eval do
   create_table :mariadb_items, force: true do |t|
-    t.blob :embedding
+    t.blob :embedding, null: false
   end
+
+  # TODO fix
+  # add_index :mariadb_items, :embedding, type: :vector
 end
 
 class MariadbItem < MariadbRecord
