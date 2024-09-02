54769a6bb29b2e1e166ed21e50b75d07ff8c8c81
Andrew Kane <andrew@ankane.org>
2024-09-02 16:42:03 -0700

    Added rrf test [skip ci]

--- test/reranking_test.rb
+++ test/reranking_test.rb
@@ -16,4 +16,11 @@ class RerankingTest < Minitest::Test
     assert_equal items[1], results[2][:result]
     assert_in_delta 0.01587, results[2][:score], 0.00001
   end
+
+  def test_rrf_k
+    create_items(Item, :embedding)
+    results = Neighbor::Reranking.rrf(["A", "B", "C"], k: 0)
+    assert_equal ["A", "B", "C"], results.map { |v| v[:result] }
+    assert_elements_in_delta [1, 0.5, 1 / 3.0], results.map { |v| v[:score] }
+  end
 end
