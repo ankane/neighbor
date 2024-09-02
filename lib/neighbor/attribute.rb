354c1f63d56561a3094ae3edddc373be4865101c
Andrew Kane <andrew@ankane.org>
2024-09-01 20:38:34 -0700

    Improved code [skip ci]

--- lib/neighbor/attribute.rb
+++ lib/neighbor/attribute.rb
@@ -1,27 +1,33 @@
 module Neighbor
   class Attribute < ActiveRecord::Type::Value
-    delegate :type, :serialize, :deserialize, :cast, to: :@cast_type
+    delegate :type, :serialize, :deserialize, :cast, to: :new_cast_type
 
     def initialize(cast_type:, model:)
-      @cast_type =
-        if cast_type.is_a?(ActiveModel::Type::Value)
-          case model.connection_db_config.adapter
-          when /sqlite/i
-            Type::SqliteVector.new
-          when /mysql|trilogy/i
-            Type::MysqlVector.new
-          else
-            cast_type
-          end
-        else
-          cast_type
-        end
+      @cast_type = cast_type
+      @model = model
     end
 
     private
 
     def cast_value(...)
-      @cast_type.send(:cast_value, ...)
+      new_cast_type.send(:cast_value, ...)
+    end
+
+    def new_cast_type
+      @new_cast_type ||= begin
+        if @cast_type.is_a?(ActiveModel::Type::Value)
+          case @model.connection_db_config.adapter
+          when /sqlite/i
+            Type::SqliteVector.new
+          when /mysql|trilogy/i
+            Type::MysqlVector.new
+          else
+            @cast_type
+          end
+        else
+          @cast_type
+        end
+      end
     end
   end
 end
