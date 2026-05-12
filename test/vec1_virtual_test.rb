16ec325df9448aac1b2ca817c3b2506f460b1035
Andrew Kane <andrew@ankane.org>
2026-05-11 17:47:31 -0700

    Improved example [skip ci]

--- test/vec1_virtual_test.rb
+++ test/vec1_virtual_test.rb
@@ -9,7 +9,7 @@ class Vec1VirtualTest < Minitest::Test
   def test_euclidean
     create_items(Vec1VirtualItem, :embedding)
 
-    items = Vec1VirtualItem.find_by_sql(["SELECT * FROM virtual_items(vec1_from_json(?), ?)", [1, 1, 1].to_json, {k: 5}.to_json])
+    items = Vec1VirtualItem.find_by_sql("SELECT * FROM virtual_items(vec1_from_json(?), ?)", [[1, 1, 1].to_json, {k: 5}.to_json])
     assert_equal [1, 3, 2], items.pluck(:id)
   end
 
@@ -17,7 +17,7 @@ class Vec1VirtualTest < Minitest::Test
     create_items(Vec1VirtualItem, :embedding)
 
     error = assert_raises(ActiveRecord::StatementInvalid) do
-      Vec1VirtualItem.find_by_sql(["SELECT * FROM virtual_items(vec1_from_json(?), ?)", [1, 1, 1].to_json, {}.to_json])
+      Vec1VirtualItem.find_by_sql("SELECT * FROM virtual_items(vec1_from_json(?), ?)", [[1, 1, 1].to_json, {}.to_json])
     end
     assert_match "no K value or visible LIMIT clause", error.message
   end
