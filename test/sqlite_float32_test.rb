d7166eaf426b62e36cb4d8b8c0f6e72b95ac0faa
Andrew Kane <andrew@ankane.org>
2026-05-13 09:20:39 -0700

    Added more tests [skip ci]

--- test/sqlite_float32_test.rb
+++ test/sqlite_float32_test.rb
@@ -33,4 +33,30 @@ class SqliteFloat32Test < Minitest::Test
     assert_equal [2, 3], result.map(&:id)
     assert_elements_in_delta [6, 4], result.map(&:neighbor_distance)
   end
+
+  def test_create
+    item = SqliteItem.create!(embedding: [1, 2, 3])
+    assert_equal [1, 2, 3], item.embedding
+  end
+
+  def test_invalid_dimensions
+    error = assert_raises(ActiveRecord::RecordInvalid) do
+      SqliteItem.create!(embedding: [1, 1])
+    end
+    assert_match "Validation failed: Embedding must have 3 dimensions", error.message
+  end
+
+  def test_infinite
+    error = assert_raises(ActiveRecord::RecordInvalid) do
+      SqliteItem.create!(embedding: [Float::INFINITY, 0, 0])
+    end
+    assert_equal "Validation failed: Embedding must have finite values", error.message
+  end
+
+  def test_nan
+    error = assert_raises(ActiveRecord::RecordInvalid) do
+      SqliteItem.create!(embedding: [Float::NAN, 0, 0])
+    end
+    assert_equal "Validation failed: Embedding must have finite values", error.message
+  end
 end
