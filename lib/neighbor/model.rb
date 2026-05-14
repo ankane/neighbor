6dd3ecb6d9affaffbc8450ba594f9415453af8ba
Andrew Kane <andrew@ankane.org>
2026-05-14 08:03:38 -0700

    Simplified code

--- lib/neighbor/model.rb
+++ lib/neighbor/model.rb
@@ -127,7 +127,7 @@ module Neighbor
           neighbor_distance =
             if distance == "cosine" && normalize_required
               "POWER(#{order}, 2) / 2.0"
-            elsif ([:vector, :halfvec, :sparsevec].include?(column_type) || adapter == :sqlite) && distance == "inner_product"
+            elsif distance == "inner_product"
               "(#{order}) * -1"
             elsif adapter == :sqlite && order.start_with?("vec1_l2_distance")
               "sqrt(#{order})"
